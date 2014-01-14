af1c753758088d15fdbc9c69185497378ef2af0c
javiakamon <franciscojavier.garcia@akamon.com>
2014-01-14 22:18:06 +0100

    Refactorización, quitamos los results en cada llamada

--- src/fizzbuzz/FizzBuzz.java
+++ src/fizzbuzz/FizzBuzz.java
@@ -7,36 +7,38 @@ public class FizzBuzz {
 
     public static String say(int number) {
         String result = "";
-        result = notSayNumber(number, result);
-        result = sayNumber(number, result);
+        result = notSayNumber(number);
+        result = sayNumberWhenTextIsEmpty(number, result);
         return result;
     }
 
-    private static String notSayNumber(int number, String result) {
-        result = sayFizz(number, result);
-        result = sayBuzz(number, result);
-        return result;
+    private static String notSayNumber(int number) {
+        String text = sayFizz(number);
+        text += sayBuzz(number);
+        return text;
     }
 
-    private static String sayNumber(int number, String text) {
-        if (text == "")
+    private static String sayNumberWhenTextIsEmpty(int number, String text) {
+        if (text.equals(""))
             text = String.valueOf(number);
         return text;
     }
 
-    private static String sayBuzz(int number, String text) {
+    private static String sayBuzz(int number) {
+        String text = "";
         if (isModule(number, MODULE_BUZZ))
             text += "Buzz";
         return text;
     }
 
-    private static String sayFizz(int number, String text) {
+    private static String sayFizz(int number) {
+        String text = "";
         if (isModule(number, MODULE_FIZZ))
             text += "Fizz";
         return text;
     }
 
     private static boolean isModule(int number, int module) {
-        return (number % module == 0);
+        return (number != 0 && number % module == 0);
     }
 }
